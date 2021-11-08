fd66f5f9a59525df320b7336868390e1dd1b828b
scalabriniF <francesca.scalabrini.s23@liceoviconapoli.it>
2021-11-08 17:32:46 +0100

    Create Scalabrini-Esercizio stringhe e liste in python-IIE-1.py

---
+++ Scalabrini-Esercizio stringhe e liste in python-IIE-1.py
@@ -0,0 +1,15 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+lista1 = ['casa', 'pappa', 'scuola', 'coding', 'python']
+len(lista1)
+5
+print(lista1[0])
+casa
+print(lista1[1])
+pappa
+print(lista1[2])
+scuola
+print(lista1[3])
+coding
+print(lista1[4])
+python

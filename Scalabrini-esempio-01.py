f45ce477a4f35e5e2c12d68e762ebc0e38973d9b
scalabriniF <francesca.scalabrini.s23@liceoviconapoli.it>
2021-11-08 17:42:14 +0100

    Create Scalabrini-esempio-01.py

---
+++ Scalabrini-esempio-01.py
@@ -0,0 +1,30 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 8
+>>> y = 67
+>>> z = 46
+>>> print(x + y + z)
+121
+>>> print(y - z - x)
+13
+>>> print(y - z + x)
+29
+>>> print(z / x)
+5.75
+>>> print(x / 2)
+4.0
+>>> print(y * z / x)
+385.25
+>>> print(z % x)
+6
+>>> print(z % 2)
+0
+>>> print(y ** x)
+406067677556641
+>>> print(x ** 2)
+64
+>>> print(y // x)
+8
+>>> print(z // 5)
+9
+>>> 

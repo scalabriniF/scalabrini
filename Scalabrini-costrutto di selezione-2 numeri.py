f773dc905a9091fb16e8564da521bc3f26425806
scalabriniF <francesca.scalabrini.s23@liceoviconapoli.it>
2021-11-15 19:55:36 +0100

    Create Scalabrini-costrutto di selezione-2 numeri.py

---
+++ Scalabrini-costrutto di selezione-2 numeri.py
@@ -0,0 +1,7 @@
+V1 = int(input('inserisci un numero '))
+V2 = int(input('inserisci un numero '))
+
+if(V1 > V2):
+    print('il maggiore dei numeri inseriti è: ', V1)
+else:
+    print('il maggiore dei numeri inseriti è: ', V2)

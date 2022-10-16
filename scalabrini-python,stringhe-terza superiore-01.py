337755680241bb2fa44dc2f6be62668f8110bd78
scalabriniF <91075681+scalabriniF@users.noreply.github.com>
2022-10-17 00:47:18 +0200

    Create scalabrini-python,stringhe-terza superiore-01.py
    esercizio: "Python, stringhe", per il 18/10/2022

---
+++ scalabrini-python,stringhe-terza superiore-01.py
@@ -0,0 +1,32 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Stringa_1 = "Esempio"
+>>> Stringa_2 = "Python"
+>>> len(Stringa_1)
+7
+>>> len(Stringa_2)
+6
+>>> len(Stringa_1) + len(Stringa_2)
+13
+>>> print(Stringa_1)
+Esempio
+>>> print(Stringa_2)
+Python
+>>> print(Stringa_1 + Stringa_2)
+EsempioPython
+>>> Stringa_1[0]
+'E'
+>>> Stringa_2[1]
+'y'
+>>> Stringa_1[3]
+'m'
+>>> Stringa_2[4]
+'o'
+>>> Stringa_1[-1]
+'o'
+>>> Stringa_2[-2]
+'o'
+>>> Stringa_1[-3]
+'p'
+>>> Stringa_2[4]
+'o'

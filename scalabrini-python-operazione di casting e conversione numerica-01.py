4d4edb19d60307cf1cb9c6da50da512f57bb4840
scalabriniF <91075681+scalabriniF@users.noreply.github.com>
2022-10-23 17:23:15 +0200

    Create scalabrini-python-operazione di casting e conversione numerica-01.py
    Operazione di casting e conversione numerica per il 25 ottobre 2022

---
+++ scalabrini-python-operazione di casting e conversione numerica-01.py
@@ -0,0 +1,20 @@
+Python 3.10.8 (tags/v3.10.8:aaaf517, Oct 11 2022, 16:50:30) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Numero1_decimale = int(478)
+>>> print(Numero1_decimale)
+478
+>>> hex(Numero1_decimale)
+'0x1de'
+>>> bin(Numero1_decimale)
+'0b111011110'
+>>> oct(Numero1_decimale)
+'0o736'
+>>> Numero1_esadecimale = 0x1de
+>>> print(Numero1_esadecimale)
+478
+>>> Numero1_binario = 0b111011110
+>>> print(Numero1_binario)
+478
+>>> Numero1_ottale = 0o736
+>>> print(Numero1_ottale)
+478
